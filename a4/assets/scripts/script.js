de6b14ee29d84589196ad83ef0ae3e6972c04b3b
SSScorpio <charleyguo8@Gmail.com>
2016-07-11 21:12:18 -0400

    Add files via upload

---
+++ a4/assets/scripts/script.js
@@ -0,0 +1,72 @@
+window.onload = function(){
+    var c = document.getElementById("main");
+    window.ctx = c.getContext("2d"); // Dealing with a global context is easier
+}
+
+var active = 0;
+
+document.getElementById("option1").onclick = function() {
+    reset();
+    active = 0;
+     $("#Main").removeClass("Hidden");
+}
+
+document.getElementById("option2").onclick = function() {
+    reset();
+    active = 1;
+    $("#Explore_Apps").removeClass("Hidden");
+}
+
+document.getElementById("option3").onclick = function() {
+    reset();
+    active = 2;
+    $("#My_Apps").removeClass("Hidden");
+}
+
+document.getElementById("option4").onclick = function() {
+    reset();
+    active = 3;
+    $("#Profile").removeClass("Hidden");
+}
+
+document.getElementById("option5").onclick = function() {
+    reset();
+    active = 4;
+    $("#About").removeClass("Hidden");
+}
+
+document.getElementById("option6").onclick = function() {
+    reset();
+    active = 5;
+    $("#Login").removeClass("Hidden");
+}
+
+document.getElementById("option7").onclick = function() {
+    reset();
+    active = 6;
+    $("#Search").removeClass("Hidden");
+}
+
+function reset(){
+    if(active === 0){
+        $("#Main").addClass("Hidden");
+    }
+    else if(active === 1){
+        $("#Explore_Apps").addClass("Hidden");
+    }
+    else if(active === 2){
+        $("#My_Apps").addClass("Hidden");
+    }
+    else if(active === 3){
+        $("#Profile").addClass("Hidden");
+    }
+    else if(active === 4){
+        $("#About").addClass("Hidden");
+    }
+    else if(active === 5){
+        $("#Login").addClass("Hidden");
+    }
+    else if(active === 6){
+        $("#Search").addClass("Hidden");
+    }
+}
